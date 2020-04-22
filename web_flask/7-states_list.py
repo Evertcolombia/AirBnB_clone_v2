1b488be4146ec863d6c7ee96328b3f709b4e0f19
evertcolombia <evertescalante@gmail.com>
2020-04-22 19:01:47 +0000

    adding a template that getsa data from tables and show it i the render template states_list/ folder

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,13 +5,14 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/state_list', strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def state_list():
     all_obj = storage.all(State)
     return render_template('7-states_list.html', states=all_obj)
 
 @app.teardown_appcontext
-def teardown_context_session():
+def teardown_context_session(self):
     return storage.close()
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
