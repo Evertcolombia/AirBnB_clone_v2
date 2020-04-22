b24ffebf85421d94dd0ea4fce43273c44c2e6e18
evertcolombia <evertescalante@gmail.com>
2020-04-22 23:29:12 +0000

    adding a simple static page usng what is stored on tables state and amenity

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
 from models import storage
-from models.satate import State
+from models.state import State
 from models.city import City
 from models.amenity import Amenity
 app = Flask(__name__)
@@ -13,9 +13,9 @@ def hbnb_filters():
     return render_template('10-hbnb_filters.html',
                             states=states, amenities=amenities)
 
-@app.teardown_appcontext:
+@app.teardown_appcontext
 def teardown_appcontext_session(self):
     return storage.close()
 
 if __name__ == "__main__":
-    app.run(hots='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
